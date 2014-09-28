2533caee23d4044f783ade5c38365c90196146a6
inktrocity <inktrocity@gmail.com>
2014-09-28 15:00:00 -0400

    Create eso_social.lua

---
+++ ESOMinion/eso_social.lua
@@ -0,0 +1,56 @@
+--:======================================================================================================================================================================
+--: eso_social
+--:======================================================================================================================================================================
+--: edited 9.28.2014
+
+eso_social = {}
+
+RegisterForEvent("EVENT_CHAT_MESSAGE_CHANNEL", true)
+RegisterForEvent("EVENT_AGENT_CHAT_REQUESTED", true)
+
+--:======================================================================================================================================================================
+--: event handlers
+--:======================================================================================================================================================================
+--: todo: add a friend/guild/group ignore check
+
+RegisterEventHandler("GAME_EVENT_CHAT_MESSAGE_CHANNEL",
+	function(event, eventnumber, messagetype, messagefrom, message)
+		if (messagetype == g("CHAT_CHANNEL_WHISPER")) then
+			d("eso_social -> new whisper: [" .. e("zo_strformat(<<1>>,"..messagefrom..")") .. "] " .. message)
+			e("PlaySound(New_Notification)")
+		else
+			d("eso_social -> new whisper: [" .. e("zo_strformat(<<1>>,"..messagefrom..")") .. "] " .. message)
+			e("PlaySound(New_Notification)")
+		end
+	end
+)
+
+RegisterEventHandler("GAME_EVENT_AGENT_CHAT_REQUESTED",
+	function(event, eventnumber)
+		if (e("IsAgentChatActive()")) then
+			d("eso_social -> new agent request: [" .. e("zo_strformat(<<1>>,"..event..")") .. "] " .. eventnumber)
+			e("PlaySound(New_Notification)")
+		end
+	end
+)
+
+--:======================================================================================================================================================================
+--: functions
+--:======================================================================================================================================================================
+
+function eso_social.IsFriend(messagefrom)
+	if (e("IsFriend("..messagefrom..")")) then
+		return true
+	else
+		for index = 1, e("GetNumFriends()") do
+			local accountname = e("GetFriendInfo("..tostring(index)..")")
+			local _, charactername = e("GetFriendCharacterInfo("..tostring(index)..")")
+			
+			if (messagefrom == accountname or messagefrom == charactername) then
+				return true
+			end
+		end
+	end
+	
+	return false
+end
