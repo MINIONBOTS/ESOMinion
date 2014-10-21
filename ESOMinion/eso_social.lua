533fa63e00dff7692f723f8fa21dd7f3360ebd2d
inktrocity <inktrocity@gmail.com>
2014-10-21 07:43:34 -0400

    Update eso_social.lua

--- ESOMinion/eso_social.lua
+++ ESOMinion/eso_social.lua
@@ -15,7 +15,7 @@ RegisterEventHandler("GAME_EVENT_CHAT_MESSAGE_CHANNEL",
 	function(event, eventnumber, messagetype, messagefrom, message)
 		if (gPlaySoundOnWhisper == "1" and tonumber(messagetype) == g("CHAT_CHANNEL_WHISPER")) then
 			d("eso_social -> new whisper: [" .. e("zo_strformat(<<1>>,"..messagefrom..")") .. "] " .. message)
-			PlaySound(GetStartupPath() .. [[\alert.wav]])
+			PlaySound(GetStartupPath() .. [[\MinionFiles\Sounds\Alarm1.wav]])
 		end
 	end
 )
@@ -24,7 +24,7 @@ RegisterEventHandler("GAME_EVENT_AGENT_CHAT_REQUESTED",
 	function(event, eventnumber)
 		if (gPlaySoundOnWhisper == "1" and e("IsAgentChatActive()")) then
 			d("eso_social -> new agent request: [" .. e("zo_strformat(<<1>>,"..event..")") .. "] " .. eventnumber)
-			PlaySound(GetStartupPath() .. [[\alert.wav]])
+			PlaySound(GetStartupPath() .. [[\MinionFiles\Sounds\Alarm1.wav]])
 		end
 	end
 )
