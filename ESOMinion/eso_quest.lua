bf1e90b7ced2dd925b69f8d5a69595353af59f19
nunyabidness <nunya@bidness.com>
2015-05-05 15:13:35 -0600

    no message

--- ESOMinion/eso_quest.lua
+++ ESOMinion/eso_quest.lua
@@ -5,9 +5,13 @@ function eso_quest_helpers.ModuleInit()
 	QuestManager.GetNearestObjectiveCallback = eso_quest_helpers.GetNearestQuestObjective
 
 	--add tasks for quest objectives
-	QuestManager.AddObjectiveTask("quest_start", eso_task_quest_start.Create)
-	QuestManager.AddObjectiveTask("quest_condition_interact", eso_task_quest_condition_interact.Create)
-	QuestManager.AddObjectiveTask("quest_condition_talkto", eso_task_quest_condition_talkto.Create)
+	QuestManager.AddObjectiveTask("MAP_PIN_TYPE_QUEST_OFFER", 								eso_task_quest_start.Create)
+	QuestManager.AddObjectiveTask("QUEST_CONDITION_TYPE_INTERACT_MONSTER", 					eso_task_quest_condition_interact.Create)
+	QuestManager.AddObjectiveTask("QUEST_CONDITION_TYPE_INTERACT_OBJECT", 					eso_task_quest_condition_interact.Create)
+	QuestManager.AddObjectiveTask("QUEST_CONDITION_TYPE_INTERACT_OBJECT_IN_STATE", 			eso_task_quest_condition_interact.Create)
+	QuestManager.AddObjectiveTask("QUEST_CONDITION_TYPE_INTERACT_SIMPLE_OBJECT", 			eso_task_quest_condition_interact.Create)
+	QuestManager.AddObjectiveTask("QUEST_CONDITION_TYPE_INTERACT_SIMPLE_OBJECT_IN_STATE", 	eso_task_quest_condition_interact.Create)
+	QuestManager.AddObjectiveTask("QUEST_CONDITION_TYPE_TALK_TO", 							eso_task_quest_condition_talkto.Create)
 end
 
 function eso_quest_helpers.GetNearestQuestObjective()
@@ -18,7 +22,8 @@ function eso_quest_helpers.GetNearestQuestObjective()
 	--start with quest conditions
 	local objective = eso_quest_helpers.GetNearestQuestCondition()
 	if(ValidTable(objective)) then
-		nearestObjective = {["type"] = "QUEST_CONDITION", ["objective"] = objective}
+		objective.type = eso_quest_helpers.ConditionTypes[objective.type]
+		nearestObjective = objective
 		nearestDistance = Distance2D(playerPos.x, playerPos.y, playerPos.z, objective.pos.x, objective.pos.y, objective.pos.z)
 	end
 	
@@ -27,7 +32,8 @@ function eso_quest_helpers.GetNearestQuestObjective()
 	if(ValidTable(offerPin)) then
 		local distance = Distance2D(playerPos.x, playerPos.y, playerPos.z, offerPin.pos.x, offerPin.pos.y, offerPin.pos.z)
 		if(distance < nearestDistance) then
-			nearestObjective = {["type"] = "QUEST_OFFER", ["objective"] = offerPin}
+			offerPin.type = eso_quest_helpers.PinTypes[offerPin.type]
+			nearestObjective = offerPin
 			nearestDistance = distance
 		end
 	end
@@ -37,13 +43,14 @@ function eso_quest_helpers.GetNearestQuestObjective()
 	if(ValidTable(endingPin)) then
 		local distance = Distance2D(playerPos.x, playerPos.y, playerPos.z, endingPin.pos.x, endingPin.pos.y, endingPin.pos.z)
 		if(distance < nearestDistance) then
-			nearestObjective = {["type"] = "QUEST_ENDING", ["objective"] = endingPin}
+			endingPin.type = eso_quest_helpers.PinTypes[endingPin.type]
+			nearestObjective = endingPin
 			nearestDistance = distance
 		end
 	end
 	
 	if(ValidTable(nearestObjective)) then
-		local oPos = nearestObjective.objective.pos
+		local oPos = nearestObjective.pos
 		local oType = nearestObjective.type
 		d("Found objective of type "..tostring(oType).." at ("..tostring(oPos.x)..","..tostring(oPos.y)..","..tostring(oPos.z)..")")
 		return nearestObjective
@@ -60,10 +67,15 @@ function eso_quest_helpers.GetNearestQuestCondition()
 		local condition = QuestManager:GetQuestCondition(cVals.JournalIndex, cVals.StepIndex, cVals.ConditionIndex)
 		--if GetQuestCondition does not return a table then the quest is complete - it will be picked up by the GetNearestPin functions
 		if(ValidTable(condition)) then
-			--if(NavigationManager:IsOnMesh(condition.pos)) then
+			if(NavigationManager:IsOnMesh(condition.pos)) then
 				d("Nearest quest condition at ("..tostring(condition.pos.x)..","..tostring(condition.pos.y)..","..tostring(condition.pos.z)..")")
+				
+				--build query table, this will be used to check for available static data
+				local questid = QuestManager.GetQuestId(cVals.JournalIndex)
+				condition["queryTable"] = {["questid"] = questid, ["conditionid"] = condition.id}
+				
 				return condition
-			--end
+			end
 		end
 	end
 	
@@ -130,3 +142,61 @@ function QuestManager:IsComplete(journalIndex)
 end
 
 RegisterEventHandler("Module.Initalize",eso_quest_helpers.ModuleInit)
+
+eso_quest_helpers.ConditionTypes = {}
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_ABILITY_TYPE_USED_ON_NPC")] = 					"QUEST_CONDITION_TYPE_ABILITY_TYPE_USED_ON_NPC"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_ABILITY_TYPE_USED_ON_TABLE")] = 				"QUEST_CONDITION_TYPE_ABILITY_TYPE_USED_ON_TABLE"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_ABILITY_USED_ON_NPC")] = 						"QUEST_CONDITION_TYPE_ABILITY_USED_ON_NPC"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_ABILITY_USED_ON_TABLE")] = 					"QUEST_CONDITION_TYPE_ABILITY_USED_ON_TABLE"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_ADVANCE_COMPLETABLE_SIBLINGS")] = 				"QUEST_CONDITION_TYPE_ADVANCE_COMPLETABLE_SIBLINGS"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_ARTIFACT_CAPTURED")] = 						"QUEST_CONDITION_TYPE_ARTIFACT_CAPTURED"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_ARTIFACT_RETURNED")] = 						"QUEST_CONDITION_TYPE_ARTIFACT_RETURNED"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_CAPTURE_KEEP_TYPE")] = 						"QUEST_CONDITION_TYPE_CAPTURE_KEEP_TYPE"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_CAPTURE_SPECIFIC_KEEP")] = 					"QUEST_CONDITION_TYPE_CAPTURE_SPECIFIC_KEEP"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_COLLECT_ITEM")] = 								"QUEST_CONDITION_TYPE_COLLECT_ITEM"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_CRAFT_ITEM")] = 								"QUEST_CONDITION_TYPE_CRAFT_ITEM"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_DECONSTRUCT_ITEM")] = 							"QUEST_CONDITION_TYPE_DECONSTRUCT_ITEM"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_DEPRECATED1")] = 								"QUEST_CONDITION_TYPE_DEPRECATED1"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_ENTER_SUBZONE")] = 							"QUEST_CONDITION_TYPE_ENTER_SUBZONE"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_EQUIP_ITEM")] = 								"QUEST_CONDITION_TYPE_EQUIP_ITEM"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_EVENT_FAIL")] = 								"QUEST_CONDITION_TYPE_EVENT_FAIL"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_EVENT_SUCCESS")] = 							"QUEST_CONDITION_TYPE_EVENT_SUCCESS"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_EXIT_SUBZONE")] = 								"QUEST_CONDITION_TYPE_EXIT_SUBZONE"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_FOLLOWER_GAINED")] = 							"QUEST_CONDITION_TYPE_FOLLOWER_GAINED"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_FOLLOWER_LOST")] = 							"QUEST_CONDITION_TYPE_FOLLOWER_LOST"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_GATHER_ITEMS")] = 								"QUEST_CONDITION_TYPE_GATHER_ITEMS"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_GIVE_ITEM")] = 								"QUEST_CONDITION_TYPE_GIVE_ITEM"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_GOTO_POINT")] = 								"QUEST_CONDITION_TYPE_GOTO_POINT"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_HAS_ITEM")] = 									"QUEST_CONDITION_TYPE_HAS_ITEM"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_INTERACT_MONSTER")] = 							"QUEST_CONDITION_TYPE_INTERACT_MONSTER"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_INTERACT_OBJECT")] = 							"QUEST_CONDITION_TYPE_INTERACT_OBJECT"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_INTERACT_OBJECT_IN_STATE")] = 					"QUEST_CONDITION_TYPE_INTERACT_OBJECT_IN_STATE"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_INTERACT_SIMPLE_OBJECT")] = 					"QUEST_CONDITION_TYPE_INTERACT_SIMPLE_OBJECT"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_INTERACT_SIMPLE_OBJECT_IN_STATE")] = 			"QUEST_CONDITION_TYPE_INTERACT_SIMPLE_OBJECT_IN_STATE"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_KILL_ENEMY_GUARDS")] = 						"QUEST_CONDITION_TYPE_KILL_ENEMY_GUARDS"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_KILL_ENEMY_PLAYERS")] = 						"QUEST_CONDITION_TYPE_KILL_ENEMY_PLAYERS"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_KILL_ENEMY_PLAYERS_OF_CLASS")] = 				"QUEST_CONDITION_TYPE_KILL_ENEMY_PLAYERS_OF_CLASS"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_KILL_ENEMY_PLAYERS_WHILE_DEFENDING_KEEP")] =	"QUEST_CONDITION_TYPE_KILL_ENEMY_PLAYERS_WHILE_DEFENDING_KEEP"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_KILL_MONSTER")] = 								"QUEST_CONDITION_TYPE_KILL_MONSTER"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_LEAVE_REVIVE_COUNTER_LIST")] = 				"QUEST_CONDITION_TYPE_LEAVE_REVIVE_COUNTER_LIST"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_NPC_GOAL")] = 									"QUEST_CONDITION_TYPE_NPC_GOAL"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_NPC_GOAL_FAIL")] = 							"QUEST_CONDITION_TYPE_NPC_GOAL_FAIL"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_PLAYER_DEATH")] = 								"QUEST_CONDITION_TYPE_PLAYER_DEATH"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_PLAYER_LOGOUT")] = 							"QUEST_CONDITION_TYPE_PLAYER_LOGOUT"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_SCRIPT_ACTION")] = 							"QUEST_CONDITION_TYPE_SCRIPT_ACTION"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_TALK_TO")] = 									"QUEST_CONDITION_TYPE_TALK_TO"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_TIMER")] = 									"QUEST_CONDITION_TYPE_TIMER"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_TRANSITION_INTERACT_OBJECT")] = 				"QUEST_CONDITION_TYPE_TRANSITION_INTERACT_OBJECT"
+eso_quest_helpers.ConditionTypes[g("QUEST_CONDITION_TYPE_USE_QUEST_ITEM")] = 							"QUEST_CONDITION_TYPE_USE_QUEST_ITEM"
+
+eso_quest_helpers.PinTypes = {}
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_ASSISTED_QUEST_CONDITION")] = 			"MAP_PIN_TYPE_ASSISTED_QUEST_CONDITION"
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_ASSISTED_QUEST_ENDING")]	= 				"MAP_PIN_TYPE_ASSISTED_QUEST_ENDING"
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_ASSISTED_QUEST_OPTIONAL_CONDITION")] =	"MAP_PIN_TYPE_ASSISTED_QUEST_OPTIONAL_CONDITION"
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_QUEST_COMPLETE")] = 						"MAP_PIN_TYPE_QUEST_COMPLETE"
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_QUEST_INTERACT")] = 						"MAP_PIN_TYPE_QUEST_INTERACT"
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_QUEST_OFFER")] = 						"MAP_PIN_TYPE_QUEST_OFFER"
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_QUEST_TALK_TO")] = 						"MAP_PIN_TYPE_QUEST_TALK_TO"
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_TRACKED_QUEST_CONDITION")] = 			"MAP_PIN_TYPE_TRACKED_QUEST_CONDITION"
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_TRACKED_QUEST_ENDING")] = 				"MAP_PIN_TYPE_TRACKED_QUEST_ENDING"
+eso_quest_helpers.PinTypes[g("MAP_PIN_TYPE_TRACKED_QUEST_OPTIONAL_CONDITION")] = 	"MAP_PIN_TYPE_TRACKED_QUEST_OPTIONAL_CONDITION"
