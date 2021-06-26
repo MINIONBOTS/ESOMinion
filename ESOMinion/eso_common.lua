a4119ddf3c1cfa8d11bbf93a793ca79943f71dd7
MarudeDameO <33278678+MarudeDameO@users.noreply.github.com>
2021-06-27 03:03:06 +0900

    unstuck stuff

---
+++ ESOMinion/eso_common.lua
@@ -0,0 +1,22 @@
+---@ for common features
+EsoCommon = {}
+-- todo:polish ui rough sinse powder rush me
+function BehaviorManager.SettingsUI()
+
+    GUI:Text("")
+    GUI:Text("UI rough. please polish. (powder rushes me so). ")
+    GUI:Text("BehaviorManager.SettingsUI inside eso_common.lua")
+    GUI:Text("")
+    if GUI:Button("Close Btree Settings") then
+        BehaviorManager.settings_open = false
+    end
+    Settings.Global.show_btree_editor = GUI:Checkbox("show btree editor ## btreeminion", Settings.Global.show_btree_editor)
+    Settings.Global.show_btree_manager = GUI:Checkbox("show btree manager ## btreeminion", Settings.Global.show_btree_manager)
+end
+
+--todo : maybe need more like pause passive tree and that
+function EsoCommon.StopTaskWithPopUp(str)
+    BehaviorManager:Stop()
+    Player:StopMovement()
+    eso_dialog_manager.IssueNotice(str)
+end
