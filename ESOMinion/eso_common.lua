c51392cd1a9a98380b77b976656829d73d11a917
fxfire <minionoffice@gmail.com>
2021-07-24 16:45:16 +0200

    fixed eso's main btree drawing ...but it should actually be drawn in each active btree and not statically from a single file or code for all btrees ...

--- ESOMinion/eso_common.lua
+++ ESOMinion/eso_common.lua
@@ -1,7 +1,8 @@
 ---@ for common features
 EsoCommon = {}
 -- todo:polish ui rough sinse powder rush me
-function EsoCommon.BtreeSettingsUI()
+-- This should instead be written in the actual Btree that is loaded, not by code ...
+function EsoCommon.DrawBTreeMainMenu()
     GUI:Text("")
     GUI:Text("UI rough. please polish. (powder rushes me so). ")
     GUI:Text("BehaviorManager.SettingsUI inside eso_common.lua")
@@ -12,6 +13,10 @@ function EsoCommon.BtreeSettingsUI()
     Settings.Global.show_btree_editor = GUI:Checkbox("show btree editor ## btreeminion", Settings.Global.show_btree_editor)
     Settings.Global.show_btree_manager = GUI:Checkbox("show btree manager ## btreeminion", Settings.Global.show_btree_manager)
 end
+if (BTSettings) then -- setting this func to be drawn by ALL active Btrees as main menu... (that's not how it should work btw..)
+    BTSettings.SetDrawMainMenuFunction ( EsoCommon.DrawBTreeMainMenu )
+end
+
 --todo : maybe need more like pause passive tree and that
 function EsoCommon.StopTaskWithPopUp(str)
     BehaviorManager:Stop()
